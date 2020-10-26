6fcb751634778520f5bc5bb04d9d9968e61bdcb8
Basalisk <basalisk@gmail.com>
2020-10-26 14:15:57 +1030

    We learn about bandits and safety

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,8 @@
+import tempfile
+
 import nox
+
+nox.options.sessions = "tests", "lint", "safety"
 
 
 @nox.session(python=["3.8", "3.7"])
@@ -20,6 +24,7 @@ def lint(session):
     args = session.posargs or lintables
     session.install(
         "flake8",
+        "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
         "flake8-import-order",
@@ -32,3 +37,19 @@ def black(session):
     args = session.posargs or lintables
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
