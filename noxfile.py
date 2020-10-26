67c336b521ba35c936236b9fe616aeba1cb9ede7
Basalisk <basalisk@gmail.com>
2020-10-26 13:56:16 +1030

    Added linting

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,17 @@ def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+lintables = [
+    "permearly/cli.py",
+    "permearly/__init__.py",
+    "noxfile.py"
+    ]
+
+@nox.session(python = ["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or lintables
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
