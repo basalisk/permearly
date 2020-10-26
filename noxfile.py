346e7751561cf8e390524345c795a6da1a71b96e
Basalisk <basalisk@gmail.com>
2020-10-26 13:59:32 +1030

    Removed some lint

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,21 @@
 import nox
 
 
-@nox.session(python = ["3.8", "3.7"])
+@nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
 
-lintables = [
-    "permearly/cli.py",
-    "permearly/__init__.py",
-    "noxfile.py"
-    ]
 
-@nox.session(python = ["3.8", "3.7"])
+lintables = ["noxfile.py",
+             "permearly/cli.py",
+             "permearly/__init__.py",
+             ]
+
+
+@nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or lintables
     session.install("flake8")
     session.run("flake8", *args)
-
-
