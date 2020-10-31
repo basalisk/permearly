fbc9680b30e5d07ab2383b79f3e8ad997c729f32
Basalisk <basalisk@gmail.com>
2020-10-31 21:06:23 +1030

    Added mypy

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,19 @@ import nox
 nox.options.sessions = "tests", "lint", "safety"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov"]
@@ -12,7 +25,7 @@ def tests(session):
     session.run("pytest", *args)
 
 
-lintables = [
+sources = [
     "noxfile.py",
     "permearly/cli.py",
     "permearly/__init__.py",
@@ -21,7 +34,7 @@ lintables = [
 
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
-    args = session.posargs or lintables
+    args = session.posargs or sources
     session.install(
         "flake8",
         "flake8-bandit",
@@ -34,7 +47,7 @@ def lint(session):
 
 @nox.session(python=["3.8", "3.7"])
 def black(session):
-    args = session.posargs or lintables
+    args = session.posargs or sources
     session.install("black")
     session.run("black", *args)
 
@@ -53,3 +66,10 @@ def safety(session):
         )
         session.install("safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=["3.8", "3.7"])
+def mypy(session):
+    args = session.posargs or sources
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
